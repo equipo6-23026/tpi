c19ef1403598cdb46d277d9fae0788c4b09d1e52
cristian <toccicristian@hotmail.com>
2023-07-03 21:58:34 -0300

    crear diseño para crud

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
-document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
-navbar-light bg-light">
+document.getElementById("header").innerHTML=` 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=VT323" rel="stylesheet">
+<link rel="stylesheet" href="styles/styles.css">
+<nav class="navbar navbar-expand-sm navbar-light bg-light">
 <div class="container">
-<a class="navbar-brand" href="../templates/productos.html">CRUD</a>
+<a class="navbar-brand" href="../templates/productos.html">SARASA CRUD</a>
 
 </div>
 </nav>`
